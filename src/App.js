3ab961c963ba89bd82dc164a887f84d7469e84eb
Noru <akang8150@naver.com>
2022-12-11 12:04:24 +0000

    Many Buttons: before styd.compt

--- src/App.js
+++ src/App.js
@@ -4,22 +4,21 @@ const Father = styled.div`
   display: flex;
 `;
 
-const Btn = styled.button`
-  color: white;
+const Input = styled.input`
   background-color: tomato;
-  border: 0;
-  border-radius: 15px;
-`;
-
-const Link = styled(Btn)`
 
 `;
 
 function App() {
   return (
     <Father as="header">
-      <Btn>Log in</Btn>
-      <Btn as="a" href="/">Log in</Btn>
+      <Input required />
+      <Input required />
+      <Input required />
+      <Input required />
+      <Input required />
+      <Input required />
+      <Input required />
     </Father>
   );
 }
