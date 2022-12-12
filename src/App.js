100f61adec55d2c29d7eb716b2aa97e28ca67bb7
Noru <akang8150@naver.com>
2022-12-12 12:28:29 +0000

    Light & Dark Mode: Theme

--- src/App.js
+++ src/App.js
@@ -1,52 +1,21 @@
-import styled, { keyframes } from "styled-components";
+import styled from "styled-components";
+
+const Title = styled.h1`
+  color: ${(props) => props.theme.textColor};
+`;
 
 const Wrapper = styled.div`
   display:flex;
   height: 100vh;
   justify-content: center;
   align-items: center;
-`;
-
-const rotationAnimation = keyframes`
-  0% {
-    transform: rotate(0deg);
-    border-radius: 25px;
-  }
-  50% {
-    transform: rotate(180deg);
-    border-radius: 75px;
-  }
-  100% {
-    transform: rotate(360deg);
-    border-radius: 25px;
-  }
-`;
-
-const Emoji = styled.span`
-    font-size: 36px;
-`;
-
-const Box = styled.div`
-  width: 200px;
-  height: 200px;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  background-color: tomato;
-  animation: ${rotationAnimation} .9s linear infinite;
-  ${Emoji} {
-    &:hover {
-      font-size: 98px;
-    }
-  }
+  background-color: ${(props) => props.theme.backgroundColor};
 `;
 
 function App() {
   return (
     <Wrapper>
-      <Box>
-        <Emoji as="p">😝</Emoji>
-      </Box>
+      <Title>Hello</Title>
     </Wrapper>
   );
 }
